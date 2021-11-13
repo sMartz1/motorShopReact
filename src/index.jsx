517b86d3c043cd444d4623b2e68b5c8e40a4ccbe
Sebastian M <ra3gan1991@gmail.com>
2021-11-13 13:48:23 +0100

    Add functionality to sort by type

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,137 @@ import ReactDOM from 'react-dom';
 import './main.css';
 import logo from './images/logo.png';
 import images from './images.js';
+const navItemsLeft = [
+  "Inicio",
+  "Carretera",
+  "Ciudad",
+  "Off Road",
+  "Trall",
+  "Trial",
+  "Casual",
+  "Accesorios",
+];
+const navItemsRight = ["Ayuda", "Nuestras tiendas", "Contacto"];
+let arrayCat = [
+  "Botas",
+  "Cascos",
+  "Chaquetas",
+  "Guantes",
+  "Impermeables",
+  "Intercomunicadores",
+  "Monos",
+  "Pantalones",
+  "Protecciones",
+  "Maletas rigidas",
+  "Maletas blandas",
+  "Antirrobos",
+  "Estriberas",
+  "Manillares",
+  "Porta matriculas",
+  "Sistemas de escape",
+  "Neumáticos",
+  "Baterías",
+];
+let arrayHelmetImg = [
+  ["AGV Skyline Block", images.casco1],
+  ["Schuberth SR1", images.casco2],
+  ["Arai Quantum DNA", images.casco3],
+  ["Shoei XR1100 Enigm TC-4", images.casco4],
+];
 
+let arrayItems = [
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item1,
+    price: 55,
+    type: "Chaqueta"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item2,
+    price: 55,
+    type: "Casco"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item3,
+    price: 55,
+    type: "Chaqueta"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item4,
+    price: 55,
+    type: "Botas"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item5,
+    price: 55,
+    type: "Pantalones"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item6,
+    price: 55,
+    type: "Casco"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item7,
+    price: 55,
+    type: "Casco"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item8,
+    price: 55,
+    type: "Casco"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item9,
+    price: 55,
+    type: "Casco"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item10,
+    price: 55,
+    type: "Guantes"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item11,
+    price: 55,
+    type: "Guantes"
+  },
+  {
+    title: "Alpinestars GP-PRO",
+    image: images.item12,
+    price: 55,
+    type: "Chaqueta"
+  },
+];
+let arrItems = [
+  "Aviso legal",
+  "Política de privacidad",
+  "Gastos de envío",
+  "Sistemas de pago",
+  "Plazos de entrega",
+  "Devoluciones",
+];
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navItemsLeft : navItemsLeft,
+      navItemsRight : navItemsRight
+    }
+  }
   
   render(){
-    const navItemsLeft = ["Inicio","Carretera","Ciudad","Off Road","Trall","Trial","Casual","Accesorios"];
-    const navItemsRight =["Ayuda","Nuestras tiendas","Contacto"];
+    
     return (
       <header>
         <div className="headSup">
@@ -17,22 +142,28 @@ class Header extends React.Component {
             <p>SHOP</p>
             <img src={logo} alt="" />
           </div>
-          <NavBar classNav="menuSup" items={navItemsRight}/>
+          <NavBar classNav="menuSup" items={this.state.navItemsRight}/>
          
         </div>
-        <NavBar items={navItemsLeft} />
+        <NavBar items={this.state.navItemsLeft} />
         </header>
     );
   }
 }
 class NavBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items : this.props.items
+    }
+  }
   render(){
     
     if(this.props.classNav === "menuSup"){
-      console.log("in")
+     
       return <><div className="menuSup">
         {
-          this.props.items.map((x,i)=>{
+          this.state.items.map((x,i)=>{
             return <div className="nav-item" key={"aMenu" + i}>{x}</div>
           })
         }
@@ -40,7 +171,7 @@ class NavBar extends React.Component {
     }else{
       return <><nav>
         {
-          this.props.items.map((x,i)=>{
+          this.state.items.map((x,i)=>{
             return <div className="nav-item"key={"bMenu" + i}>{x}</div>
           })
         }
@@ -62,17 +193,19 @@ class Featured extends React.Component {
 }
 
 class MostSold extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayHelmetImg : arrayHelmetImg
+    }
+  }
   render(){
-    let arrayHelmetImg =  [
-      ["AGV Skyline Block",images.casco1],
-      ["Schuberth SR1",images.casco2],
-      ["Arai Quantum DNA",images.casco3],
-      ["Shoei XR1100 Enigm TC-4",images.casco4]];
+    
     
     return (<><section className="featured-items">
     <div className="titleFI">Artículos destacados</div>
     <div className="imagesFI">
-        {arrayHelmetImg.map((x,i)=>{
+        {this.state.arrayHelmetImg.map((x,i)=>{
           return <ItemFeatured title={x[0]} image={x[1]} key={"sold-" + i} />
         })}
        </div>
@@ -81,10 +214,17 @@ class MostSold extends React.Component {
 }
 
 class ItemFeatured extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title,
+      image : this.props.image
+    }
+  }
   render(){
     return <> <div className="itemFI">
-    <img src={this.props.image} alt="" />
-    <p>{this.props.title}</p>
+    <img src={this.state.image} alt="" />
+    <p>{this.state.title}</p>
     </div></>
   }
 }
@@ -109,31 +249,20 @@ class Shop extends React.Component {
   }
 }
 class Categories extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayCat : arrayCat
+    }
+    
+  }
   render(){
-    let arrayCat = [
-      "Botas",
-      "Cascos",
-      "Chaquetas",
-      "Guantes",
-      "Impermeables",
-      "Intercomunicadores",
-      "Monos",
-      "Pantalones",
-      "Protecciones",
-      "Maletas rigidas",
-      "Maletas blandas",
-      "Antirrobos",
-      "Estriberas",
-      "Manillares",
-      "Porta matriculas",
-      "Sistemas de escape",
-      "Neumáticos",
-      "Baterías"
-      ]
+   
     return  <aside className="categorys">
     <p>Categorías</p>
     {
-      arrayCat.map((x,i)=>{
+      this.state.arrayCat.map((x,i)=>{
         return <Category content={x} key={"cat" + i} />
       })
     }
@@ -146,11 +275,37 @@ class Category extends React.Component {
   }
 }
 class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectOptions : ["Cascos","Chaqueta","Guantes","Pantalones","Botas"],
+      arrItems: arrayItems,
+      filteredItems: arrayItems
+    }
+    this.selectOnChange = this.selectOnChange.bind(this);
+    
+  }
+   selectOnChange (e){
+    let tempFilter = this.state.arrItems.filter(item => item.type === e.target.value);
+    console.log("Antes de setState");
+    console.log(tempFilter);
+    this.setState({
+      filteredItems : tempFilter
+    });
+    console.log("Despues de setState");
+    console.log(this.state.filteredItems);
+  }
   render(){
     return <section className="results"> 
     <p>Articulos más vendidos</p>
-    <ItemFilter />
-    <ItemContainer />
+    <p className="tagC">Ordenar por: 
+    <select onChange={this.selectOnChange}>
+      {this.state.selectOptions.map((option,i)=>{
+        return <option key={i}>{option}</option>
+      })}
+    </select> 
+    </p> 
+    <ItemContainer items={this.state.filteredItems}/>
     <PageNav />
     </section>
   }
@@ -162,106 +317,82 @@ class ItemFilter extends React.Component {
 }
 class ItemContainer extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayItems : this.props.items
+    }
+    
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({ arrayItems: nextProps.items });  
+  }
   render(){
-    let arrayItems = [
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item1,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item2,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item3,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item4,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item5,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item6,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item7,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item8,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item9,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item10,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item11,
-        price:55
-      },
-      {
-        title:"Alpinestars GP-PRO",
-        image:images.item12,
-        price:55
-      }
-    ]
+    console.log("Lista de items con array : ");
+    console.log(this.state.arrayItems);
     return <div className="list-items">
-      {arrayItems.map((x,i)=>{
+      {this.state.arrayItems.map((x,i)=>{
         return <Item title={x.title} image={x.image} price={x.price} key={"item" + i} />
       })}
       </div>
   }
 }
 class Item extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        title : this.props.title,
+        image : this.props.image,
+        price : this.props.price
+
+    }
+    
+  }
   render(){
     return <div className="item">
-    <p className="item-title">{this.props.title}</p>
-    <img src={this.props.image} alt="" />
+    <p className="item-title">{this.state.title}</p>
+    <img src={this.state.image} alt="" />
     <div className="item-bottom">
         <div className="button-buy">Comprar</div>
-        <p>{this.props.price}€</p>
+        <p>{this.state.price}€</p>
     </div>
     </div>
   }
 }
 class PageNav extends React.Component {
+  
+    
+  
   render(){
+    let tempArray = [];
+     for (let i = 1; i < 9; i++) {
+      if(i === 7){
+        tempArray.push(<PageNavItem page="..." key={"page-nav-" + i} />);
+      }else if(i === 8){
+        tempArray.push(<PageNavItem page="Siguiente" key={"page-nav-" + i} />);
+      }else{
+        tempArray.push(<PageNavItem page={i} key={"page-nav-" + i} />);
+      }
+      
+    }
+   
     return  <div className="page-selection">
-      {Array(9).fill(1).map((x,i)=>{
-        if(i === 7){
-          return <PageNavItem page="..." key={"page-nav-" + i} />
-        }else if(i === 8){
-          return <PageNavItem page="Siguiente" key={"page-nav-" + i} />
-        }else{
-          return <PageNavItem page={i} key={"page-nav-" + i} />
-        }
-      })}
-</div>
+     {tempArray.map(element=>element)}
+  </div>
   }
 }
 class PageNavItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page : this.props.page
+    }
+    
+  }
   render(){
-    return <div className="page-item">{this.props.page}</div>
+    return <div className="page-item">{this.state.page}</div>
   }
 }
 
@@ -275,39 +406,60 @@ class Footer extends React.Component {
 }
 
 class MenuFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrItems : arrItems
+    }
+    
+  }
   render(){
-    let arrItems = [
-      "Aviso legal",
-      "Política de privacidad",
-      "Gastos de envío",
-      "Sistemas de pago",
-      "Plazos de entrega",
-      "Devoluciones"];
+ 
     return <div className="menu-footer">
-      {arrItems.map((x,i)=>{
+      {this.state.arrItems.map((x,i)=>{
         return <ItemMenuFooter text={x} key={"item-menu-" + i} />
       })}
     </div>
   }
 }
 class ItemMenuFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text : this.props.text
+    }
+    
+  }
   render(){
-    return <div className="footer-item">{this.props.text}</div>
+    return <div className="footer-item">{this.state.text}</div>
   }
 }
 class SocialFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrImages : [images.facebook,images.twitter,images.wifi]
+    }
+    
+  }
   render(){
-    let arrImages = [images.facebook,images.twitter,images.wifi]
     return <div className="social-footer">
-      {arrImages.map((x,i)=>{
+      {this.state.arrImages.map((x,i)=>{
         return <ItemSocialFooter image={x} key={"item-social-" + i} />
       })}
     </div>
   }
 }
 class ItemSocialFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image : this.props.image
+    }
+    
+  }
   render(){
-    return  <img src={this.props.image} alt="" />
+    return  <img src={this.state.image} alt="" />
   }
 }
 class MainContainer extends React.Component {
